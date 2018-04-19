3459824190a45f4d6db8fded13ce4ef9d46d21f9
johannamelly <johannamelly@gmail.com>
2018-04-19 20:19:29 +0200

    push caonscknsf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 
 import data.Victim;
 import data.VictimsList;
+import model.mail.Mail;
 import model.prank.PrankGenerator;
+import smtp.SmtpClient;
 
 import java.security.spec.ECField;
 import java.util.ArrayList;
@@ -14,15 +16,19 @@ public class Main {
     protected static final Logger LOG = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        SmtpClient client = new SmtpClient();
         try {
             LOG.info("Application Start");
-
            // List<String> x = new ArrayList<>(Arrays.asList("xyz", "abc"));
-            VictimsList vL;
-
-
+            client.connect("localhost");
             PrankGenerator prankGen = new PrankGenerator();
-            prankGen.generate();
+            List<Mail> pranks = prankGen.generate();
+
+            for(Mail prank : pranks){
+                client.sendMail(prank);
+            }
+
+            client.disconnect();
            // PrankGenerator pg = new PrankGenerator();
           //  pg.generate();
 
